5c6d5679a5d8a0dbfbde800c1ee21106e2907bd2
jicwang <wangjc857@gmail.com>
2020-04-20 21:51:41 +0800

    Add project files.

---
+++ SQLVsLINQ/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLVsLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LINQToSQL ins = new LINQToSQL();
+            ins.exer2();
+        }
+    }
+}
